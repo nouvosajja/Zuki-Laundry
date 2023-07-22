cde69a6f4ce71667d98ba7e7d173ac8ff66b59d2
ubeb <87508994+ubeb@users.noreply.github.com>
2023-07-22 11:35:06 +0700

    tambahAlamat.dart

---
+++ lib/profile/tambahAlamat.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class tambahAlamat extends StatefulWidget {
+  const tambahAlamat({super.key});
+
+  @override
+  State<tambahAlamat> createState() => _tambahAlamatState();
+}
+
+class _tambahAlamatState extends State<tambahAlamat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(5, 25, 0, 30),
+                      child: Material(
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(Icons.arrow_back_ios,
+                              size: 40, color: Color.fromARGB(255, 0, 0, 0)),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 30, 0, 32),
+                      width: 250,
+                      child: const Text(
+                        "Tambah Alamat",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30),
+                      ),
+                    ),
+                  ],
+                ),
+                Text('Nama Jalan'),
+                SizedBox(
+                  height: 5,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Masukkan Alamat',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text('kode pos'),
+                SizedBox(
+                  height: 5,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter your name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text('catatan'),
+                SizedBox(
+                  height: 5,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Opsional',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Tambahkan'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
